caa8cd0e3d68f860cba27e88c8e03d489d733120
Rafail <Raf_0707@mail.ru>
2024-04-27 21:07:40 +0300

    Initial commit

---
+++ src/main/kotlin/raf/console/nataschool/NataschoolApplication.kt
@@ -0,0 +1,11 @@
+package raf.console.nataschool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NataschoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<NataschoolApplication>(*args)
+}
---
+++ src/test/kotlin/raf/console/nataschool/NataschoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package raf.console.nataschool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NataschoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
